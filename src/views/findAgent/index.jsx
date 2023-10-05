f8f920898599c8afe650ff17c8396f8a0384f1bd
pankaj <pankaj@avioxtechnologies.com>
2023-10-05 14:15:53 +0530

    Fixed the filter component

--- src/views/findAgent/index.jsx
+++ src/views/findAgent/index.jsx
@@ -10,28 +10,13 @@ const FindAgent = () => {
     <>
       <section className='filter-section pt-2'>
         <Container>
-          {/* <div className='d-flex align-items-center mb-21'>
-            <div className='search-card bg-grey py-lg-2 py-1 p-3 w-100 me-3'>
-              <Row className='align-items-center'>
-                <Col lg={3} md={3}>
-                  <button className='search-by-map p-1'>Search by map</button>
-                </Col>
-                <Col lg={9} md={9}>
-                  <div className=''>
-                      <div className='d-flex align-items-center'>
-                          <input type='text' placeholder='Enter a State, Country or City' className='form-control search-field py-1' />
-                          <button className='search-btn small-search-btn'><img src={searchIcon} /></button>
-                      </div>
-                  </div>
-                </Col>
-              </Row>
-            </div>
+          <div className='d-flex align-items-center mb-21'>
             <div>
               <button className='filter-btn'>
                 <img src={filterIcon}/>Filter
               </button>
             </div>
-          </div> */}
+          </div>
           <div className='d-flex align-items-center justify-content-between mb-4'>
             <p className='text-body'>Arkansas Land for Sale - 14,342 Listings</p>
             <Form.Select className='sort-select shadow-none'>
