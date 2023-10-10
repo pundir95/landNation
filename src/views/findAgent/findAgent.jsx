d5204e6b5fac49619b91367d25ac82a408208004
shagun <shagun.sharma9889@gmail.com>
2023-10-10 14:41:18 +0530

    Filter Section

--- src/views/findAgent/findAgent.jsx
+++ src/views/findAgent/findAgent.jsx
@@ -23,6 +23,35 @@ const FindAgent = () => {
                   </button>
                 </div>
               </div>
+              <div>
+                <div className="filter-header d-flex justify-content-between align-items-center mb-3">
+                  <h3 className="filter-head font-20 fw-semibold mb-0">Filter</h3>
+                  <button className="close-btn"><img src={closeIcon} /></button>
+                </div>
+                <div className="feature-card rounded-12 p-1 mb-3">
+                  <div className="filter-subhead mb-4">
+                    Active Filters
+                  </div>
+                  <div className="mb-4">
+                    <span className="active-filter">Arkansas <button className="close-filter"><img src={closeIcon} /></button></span>
+                  </div>
+                </div>
+                <div className="feature-card rounded-12 p-1">
+                  <div className="filter-subhead mb-4">
+                    Region
+                  </div>
+                  <div className="filter-container px-3">
+                    <div className="mb-3 d-flex align-items-center justify-content-between">
+                      <p className="text-body fw-normal mb-0">Southern Region</p>
+                      <p className="text-body fw-normal mb-0">8,431</p>
+                    </div>
+                    <div className="mb-3 d-flex align-items-center justify-content-between">
+                      <p className="text-body fw-normal mb-0">Western Region</p>
+                      <p className="text-body fw-normal mb-0">5,455</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="d-flex align-items-center justify-content-between mb-4">
                 <p className="text-body">
                   Arkansas Land for Sale - 14,342 Listings
